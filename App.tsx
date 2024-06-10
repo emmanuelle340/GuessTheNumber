03a42cb9e5b82c0b4ff8c539c8b95968a5a58b63
emmanuelle340 <emmanuellekenmogne340@gmail.com>
2024-06-10 01:12:26 +0100

    my first commit

--- App.tsx
+++ App.tsx
@@ -1,95 +1,187 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import LoginPage from './src/Screen/LoginPage.tsx';
+import React, { useState, useEffect } from "react";
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
-} from 'react-native';
+  Text,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import firestore, { firebase } from "@react-native-firebase/firestore";
+import DeviceInfo from "react-native-device-info";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Icon from "react-native-vector-icons/MaterialIcons"; // If using vector icons
+import { Player } from "./src/model/Player";
+import LoginScreen from "./src/screens/LoginScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import SessionScreen from "./src/screens/SessionScreen";
+import SalleDattenteScreen from "./src/screens/SalleDattenteScreen";
+import JoinGameScreen from "./src/screens/JoinGame";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+let gameId: string;
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const generateRandomName = () => {
+  // Liste de mots qui peuvent être combinés pour former un nom de partie
+  const firstWords = [
+    "Chat",
+    "Pizza",
+    "Montagne",
+    "Jungle",
+    "Aventure",
+    "Secret",
+    "Super",
+    "Cosmique",
+    "Galaxie",
+  ];
+  const secondWords = [
+    "Rapide",
+    "Glace",
+    "Magique",
+    "Infini",
+    "Mystère",
+    "Légendaire",
+    "Explorateur",
+    "Sauvage",
+    "Ninja",
+  ];
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  // Choix aléatoire d'un mot de chaque liste
+  const firstWord = firstWords[Math.floor(Math.random() * firstWords.length)];
+  const secondWord =
+    secondWords[Math.floor(Math.random() * secondWords.length)];
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  // Combinaison des mots pour former le nom de la partie
+  const randomName = `${firstWord}${secondWord}`;
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  return randomName;
+};
+
+const App = () => {
+  const [currentScreen, setCurrentScreen] = useState("login");
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      const gamesSnapshot = await firestore()
+        .collection("Games")
+        .where("gameStatus", "==", true)
+        .get();
+      const gamesData = gamesSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setGames(gamesData);
+    };
+    if (currentScreen === "joinGame") {
+      fetchGames();
+    }
+  }, [currentScreen]);
+
+  const goToHomeScreen = () => setCurrentScreen("home");
+  const goToSessionScreen = () => setCurrentScreen("session");
+  const goToSalleDattenteScreen = () => setCurrentScreen("salleDattente");
+  const goToPreviousSessionScreen = () => setCurrentScreen("session");
+  const goToJoinGameScreen = () => setCurrentScreen("joinGame");
+
+  const createGame = async () => {
+    try {
+      const deviceId = await DeviceInfo.getUniqueId();
+      const deviceName = await DeviceInfo.getDeviceName();
+      const gameRef = await firestore().collection("Games").add({
+        createdBy: [{ deviceId, deviceName }],
+        createdAt: firestore.FieldValue.serverTimestamp(),
+        gameStatus: true,
+        GameParticipantDeviceId: [],
+        gameName: generateRandomName(),
+      });
+      console.log(`Game created with ID: ${gameRef.id}`);
+      // You can store this game ID if needed for further operations
+      goToSalleDattenteScreen();
+    } catch (error) {
+      console.error("Error creating game: ", error);
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-        <LoginPage/>
-    </SafeAreaView>
+    <View style={styles.appContainer}>
+      {currentScreen === "login" && <LoginScreen onLogin={goToHomeScreen} />}
+      {currentScreen === "home" && <HomeScreen onNext={goToSessionScreen} />}
+      {currentScreen === "session" && (
+        <SessionScreen
+          onNext={goToJoinGameScreen}
+          onBack={goToHomeScreen}
+          createGame={createGame}
+        />
+      )}
+      {currentScreen === "salleDattente" && (
+        <SalleDattenteScreen onBack={goToPreviousSessionScreen} onNext={undefined} />
+      )}
+      {currentScreen === "joinGame" && (
+        <JoinGameScreen games={games} onBack={goToSessionScreen} onNext={goToSalleDattenteScreen} />
+      )}
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  appContainer: {
+    flex: 1,
   },
-  sectionTitle: {
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 50, // To ensure there is space at the top for the back button
+  },
+  text: {
     fontSize: 24,
-    fontWeight: '600',
+    marginBottom: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
+  backButton: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+    padding: 10,
+  },
+
+  listContainer: {
+    paddingVertical: 16,
+  },
+  gameItem: {
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    padding: 16,
+    marginHorizontal: 16,
+    marginBottom: 16,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    //height: 100,
+  },
+  verticalItem: {
+    flexDirection: "column",
+  },
+  gameItemContent: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  gameName: {
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "bold",
+    marginBottom: 8,
   },
-  highlight: {
-    fontWeight: '700',
+  createdBy: {
+    fontSize: 16,
+    marginBottom: 4,
+  },
+  createdAt: {
+    fontSize: 14,
+    color: "#666",
   },
 });
 
